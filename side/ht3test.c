b39fbe62879131e7fb77886f7594bae6610e97cd
Miron B. Kursa <m@mbq.me>
2019-09-28 22:55:18 +0200

    More messing

--- side/ht3test.c
+++ side/ht3test.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <time.h>
 
 //Shims
 char* R_alloc(size_t a,size_t b){
@@ -15,30 +16,113 @@ int min(int a,int b){
 }
 void GetRNGstate(){}
 void PutRNGstate(){}
+#define NA_INTEGER 0xffffffff
+void error(const char *e){
+ abort();
+}
 
 #include "../src/ht3.h"
+#include "../src/ht.h"
 #include "../src/pcg.h"
 
 int main(int argc,char **argv){
+ printf("mb\tn\tnx\tny\tnz\tns\tcmi\tnst\toccp\tpoccp\tmD\taD\ttime\ttimeOld\n");
  uint64_t r=21; rng(&r,0);
+ struct timespec tpa,tpb;
+ double timeOld=0.;
+ double timeNew=0.;
 
- int n=10+rng(&r,0)%30;
- int nx=min(1+rng(&r,0)%20,n);
- int ny=min(1+rng(&r,0)%20,n);
- int nz=min(1+rng(&r,0)%20,n);
- int *x=malloc(sizeof(int)*n),
-  *y=malloc(sizeof(int)*n),
-  *z=malloc(sizeof(int)*n);
- struct ht3 *H=R_allocHt3(n);
+ for(int j=0;j<1;j++){
+
+  
+  int n,nx,ny,nz;
+  if(1){
+   if(rng(&r,0)%2==0) n=50+rng(&r,0)%100; else n=100+rng(&r,0)%300;
+   nx=min(1+rng(&r,0)%50,n);
+   ny=min(1+rng(&r,0)%50,n);
+   nz=min(1+rng(&r,0)%50,n);
+  }else{
+   //Madelon
+   n=2000;
+   nx=10;
+   ny=10;
+   nz=2;
+  }
+  int *x=malloc(sizeof(int)*n),
+   *y=malloc(sizeof(int)*n),
+   *z=malloc(sizeof(int)*n);
+  struct ht3 *H=R_allocHt3(n);
+
+  struct ht *h=R_allocHt(n);
+  int *xz=malloc(sizeof(int)*n);
+  int *cXZ=malloc(sizeof(int)*n);
+  int *cY=malloc(sizeof(int)*n);
+  
+  for(int e=0;e<n;e++){
+   x[e]=rng(&r,0)%nx+1;
+   y[e]=rng(&r,0)%ny+1;
+   z[e]=rng(&r,0)%nz+1;
+   int nB=5;
+   printf(">>> %d %d %d => %d %d %d %d\n",x[e],y[e],z[e],hash(x[e],y[e],z[e],nB),hash(x[e],0,z[e],nB),hash(0,y[e],z[e],nB),hash(z[e],1,z[e],nB));
+  }
  
- for(int e=0;e<n;e++){
-  x[e]=rng(&r,0)%nx+1;
-  y[e]=rng(&r,0)%ny+1;
-  z[e]=rng(&r,0)%nz+1;
- }
+  clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&tpb);
+  double timeHt3=0.; int ni=0;
+  int ns; double cmi;
+  while(timeHt3<10000){
+   ni++;
+   ns=fillHt3(H,n,nx,x,ny,y,nz,z);
+   cmi=cmiHt3(H,ns,n);
+   clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&tpa);
+   timeHt3=((tpa.tv_sec-tpb.tv_sec)*1e9+(tpa.tv_nsec-tpb.tv_nsec));
+  }
+  timeHt3/=((double)ni)*((double)n);
+  int numOccupied=0;
+  int maxDepth=0;
+  double aveDepth=0;
+  for(int e=0;e<(1<<H->mBits);e++){
+   numOccupied+=!!H->map[e];
+   int depth=0;
+   for(struct ht3e *a=H->map[e];a;a=a->nxt) depth++;
+   aveDepth+=depth;
+   maxDepth=MAX(maxDepth,depth);
+  }
+  double occupation=((double)numOccupied)/((double)(1<<H->mBits));
+  aveDepth/=numOccupied;
 
- int ns=fillHt3(H,n,nx,x,ny,y,nz,z);
- printf("CMI[%d]=%0.3f\n",ns,cmiHt3(H,ns,n));
+  //Alternative ht
+  clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&tpb);
+  double timeHt=0.; ni=0;
+  while(timeHt<10000){
+   ni++;
+   int nxz=fillHt(h,n,nz,z,nx,x,xz,NULL,NULL,1);
+   fillHt(h,n,ny,y,nxz,xz,NULL,cY,cXZ,0); //filling cY is a bit cheating
+   miHt(h,cY,cXZ);
+   clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&tpa);
+   timeHt=((tpa.tv_sec-tpb.tv_sec)*1e9+(tpa.tv_nsec-tpb.tv_nsec));
+  }
+  timeHt/=((double)ni)*((double)n);
+
+  timeNew+=timeHt3;
+  timeOld+=timeHt;
+
+
+  printf("%d\t%d\t%d\t%d\t%d\t%d\t%0.3f\t%d\t%d\t%0.3f\t%d\t%0.3f\t%0.6f\t%0.6f\n",H->mBits,n,nx,ny,nz,ns,cmi,ns,numOccupied,occupation,maxDepth,aveDepth,timeHt3,timeHt);
+
+  free(x);
+  free(y);
+  free(z);
+  free(H->counts);
+  free(H->contents);
+  free(H->map);
+  free(H);
+
+  free(xz);
+  free(h->map);
+  free(h->cnt);
+  free(h);
+ }
+ fprintf(stderr,"Slowdown: %0.3f\n",timeNew/timeOld);
  
  return(0);
 }
--- src/ht3.h
+++ src/ht3.h
@@ -1,7 +1,11 @@
+#define MAX(a,b) ((a)<(b))?(b):(a)
+#define MIN(a,b) ((a)>(b))?(b):(a)
+
 struct ht3{
  struct ht3e *contents; //size N
  struct ht3e **map;
  int *counts;
+ int mBits;
 };
 
 struct ht3e{
@@ -17,60 +21,58 @@ struct ht3e{
 
 struct ht3* R_allocHt3(int N){
  struct ht3 *ans=(struct ht3*)R_alloc(sizeof(struct ht3),1);
- ans->map=(struct ht3e**)R_alloc(sizeof(struct ht3e*),1024);
  ans->contents=(struct ht3e*)R_alloc(sizeof(struct ht3e),N);
- ans->counts=(int*)R_alloc(sizeof(int),N);
+ ans->counts=(int*)R_alloc(sizeof(int),N*4);
+ int mB=1;
+ for(;(1<<mB)<=N;mB++);
+ ans->mBits=MIN(14,mB+2);
+ ans->map=(struct ht3e**)R_alloc(sizeof(struct ht3e*),1<<(ans->mBits));
  return(ans);
 }
 
-int static inline disagree(struct ht3e *E,int x,int y,int z){
- return(!(
-  ((x==0) || E->x==x) &&
-  ((y==0) || E->y==y) &&
-  ((z==0) || E->z==z)
- ));
-}
-int static inline hash(int x,int y,int z,int n){
- //return((x+y+z) & 0x3ff);
- //return(x & 0x3ff);
- //return((x+(y*4)+(z*8))%n);
+int hash(int x,int y,int z,int mB){
+  return((x+y+z)%(1<<mB));
  uint64_t pp=6364136223846793005;
- //uint64_t mix=((x*pp)+y)*pp+z;
- uint64_t mix=((x*pp)+x)*pp+x;
+ uint64_t mix=((x*pp)+y)*pp+z;
  uint32_t s=((mix>>18)^mix)>>27;
  uint32_t r=mix>>59;
- return ((s<<((-r)&31))|(s>>r))>>22;
+ return ((s<<((-r)&31))|(s>>r))%(1<<mB);//>>(32-mB);
 }
 
 //x &y start from 1
 int fillHt3(struct ht3 *ht,int n,int nx,int *x,int ny,int *y,int nz,int *z){
- for(int e=0;e<1024;e++) ht->map[e]=NULL;
+ //Calculate maximal number of states
+ uint64_t mStates,acc=((uint64_t)nx+1)*((uint64_t)ny+1);
+ if(acc>0xffffffff){
+  //Too many
+  mStates=((uint64_t)1)<<63;
+ }else{
+  mStates=acc*((uint64_t)nz+1);
+ }
+ int mB=ht->mBits;
+ if(mStates<(1<<mB)){
+  printf("Shortcut possible :(\n");
+ }
+ for(int e=0;e<(1<<mB);e++) ht->map[e]=NULL;
  int nE=0,nC=0;
  int *counts=ht->counts;
 
  for(int e=0;e<n;e++){
-  int h=hash(x[e],y[e],z[e],n);
+  int X=x[e],Y=y[e],Z=z[e];
   struct ht3e **E;
-  for(E=ht->map+h;
-   (*E) &&
-   x[e]!=(*E)->x &&
-   y[e]!=(*E)->y &&
-   z[e]!=(*E)->z;
-   E=&((*E)->nxt)
-  );
+  for(E=ht->map+hash(X,Y,Z,mB);(*E) && (X!=(*E)->x || Y!=(*E)->y || Z!=(*E)->z);E=&((*E)->nxt));
 
   if(!*E){
    //E not found, adding!
    *E=ht->contents+nE; nE++;
    (*E)->cXYZ=0;
    (*E)->nxt=NULL;
-   (*E)->x=x[e];
-   (*E)->y=y[e];
-   (*E)->z=z[e];
+   (*E)->x=X; (*E)->y=Y; (*E)->z=Z;
 
    //Link XZ
    struct ht3e **EE;
-   for(EE=ht->map+hash(x[e],0,z[e],n);(*EE)&&disagree(*EE,x[e],0,z[e]);EE=&((*EE)->nxt)); 
+   for(EE=ht->map+hash(X,0,Z,mB);(*EE) && (X!=(*EE)->x || Z!=(*EE)->z);EE=&((*EE)->nxt));
+
    if(!*EE || *EE==*E){
     *EE=*E;
     (*E)->cXZ=counts+nC; nC++;
@@ -78,7 +80,7 @@ int fillHt3(struct ht3 *ht,int n,int nx,int *x,int ny,int *y,int nz,int *z){
    }else (*E)->cXZ=(*EE)->cXZ;
  
    //Link YZ
-   for(EE=ht->map+hash(0,y[e],z[e],n);(*EE)&&disagree(*EE,0,y[e],z[e]);EE=&((*EE)->nxt)); 
+   for(EE=ht->map+hash(0,Y,Z,mB);(*EE) && (Y!=(*EE)->y || Z!=(*EE)->z);EE=&((*EE)->nxt));
    if(!*EE || *EE==*E){
     *EE=*E;
     (*E)->cYZ=counts+nC; nC++;
@@ -86,7 +88,7 @@ int fillHt3(struct ht3 *ht,int n,int nx,int *x,int ny,int *y,int nz,int *z){
    }else (*E)->cYZ=(*EE)->cYZ;
 
    //Link Z
-   for(EE=ht->map+hash(0,0,z[e],n);(*EE)&&disagree(*EE,0,0,z[e]);EE=&((*EE)->nxt)); 
+   for(EE=ht->map+hash(0,0,Z,mB);(*EE) && Z!=(*EE)->z;EE=&((*EE)->nxt));
    if(!*EE || *EE==*E){
     *EE=*E;
     (*E)->cZ=counts+nC; nC++;
