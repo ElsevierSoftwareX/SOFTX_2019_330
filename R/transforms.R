5a09c940d6d626eada3e8711fe13716dc7b00653
Miron B. Kursa <m@mbq.me>
2020-01-09 21:41:52 +0100

    More meh

--- R/transforms.R
+++ R/transforms.R
@@ -4,7 +4,7 @@
 #' 
 #' @param x Vector or data frame to be Kendall-transformed.
 #' @examples
-#' kTransform(data.frame(Asc=1:3,Desc=3:1,Vsh=c(2,1,2))
+#' kTransform(data.frame(Asc=1:3,Desc=3:1,Vsh=c(2,1,2)))
 #' @export
 kTransform<-function(x)
  if(is.data.frame(x)) data.frame(.Call(C_kt,x)) else .Call(C_kt,x)
