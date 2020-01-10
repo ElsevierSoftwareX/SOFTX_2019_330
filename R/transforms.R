85948a23f2398ae67b43b60aae8e6984d92b75b1
Miron B. Kursa <m@mbq.me>
2020-01-10 01:13:57 +0100

    Documentation update

--- R/transforms.R
+++ R/transforms.R
@@ -2,7 +2,9 @@
 
 #' Kendall transformation
 #' 
-#' @param x Vector or data frame to be Kendall-transformed.
+#' @param x Vector or data frame to be Kendall-transformed; allowed feature types are numeric, integer (treated as numeric), ordered factor, logical and unordered factor with two or less levels.
+#' \code{NA} and non-finite values are allowed; \code{NaN} is treated as \code{NA}.
+#' @return A transformed vector or data frame with transformed columns.
 #' @examples
 #' kTransform(data.frame(Asc=1:3,Desc=3:1,Vsh=c(2,1,2)))
 #' @export
